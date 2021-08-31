bb0f8581416f6287b500ebf7a4ac4a6d9c645612
Mingmar Dolma <62723387+mingmardolma@users.noreply.github.com>
2021-08-31 10:13:48 -0400

    Delete program1.py

--- program1.py
+++
@@ -1,9 +0,0 @@
-"""
-Name: Mingmar Dolma
-Email: Mingmar.Dolma17@myhunter.cuny.edu
-Date: August 24, 2021
-This program prints: Hello, World!
-Resources: used csci 127 to review python
-"""
-
-print("Hello, World")
